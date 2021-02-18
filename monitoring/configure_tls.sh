e75d90319e4723feff60e7e99061c06430cae74e
LeChatErrant <guillaume.hector@epitech.eu>
2021-02-18 17:49:18 +0100

    feat(monitoring): elasticsearch certificates now generated in PEM format

--- monitoring/configure_tls.sh
+++ monitoring/configure_tls.sh
@@ -34,39 +34,32 @@ espod=$(kubectl get pods -n monitoring | grep elasticsearch-statefulset | sed -n
 blue
 bold
 echo
-echo "Generating CA (Certificate Authority) for your elasticsearch cluster..."
+echo "Generating certificates..."
 normal
-yes "" | kubectl exec -it "$espod" -n monitoring -- bin/elasticsearch-certutil ca
+yes "" | kubectl exec -it "$espod" -n monitoring -- bin/elasticsearch-certutil cert -pem
 green
-echo "CA (Certificate Authority) successfully generated !"
+echo "Certificates successfully generated"
 normal
-# Enter
-# Enter
 
 blue
+echo
+echo "Downloading certificates..."
+normal
+kubectl cp monitoring/"$espod":/usr/share/elasticsearch/certificate-bundle.zip certificate-bundle.zip
+unzip certificate-bundle.zip -d elastic-certificate-bundle
+rm certificate-bundle.zip
+mv elastic-certificate-bundle/instance/instance.crt elastic-certificate-bundle/instance/tls.crt
+mv elastic-certificate-bundle/instance/instance.key elastic-certificate-bundle/instance/tls.key
+green
 bold
-echo
-echo "Generating certificate..."
+echo "Certificates successfully downloaded in monitoring/elastic-certificate-bundle !"
 normal
-yes "" | kubectl exec -it "$espod" -n monitoring -- bin/elasticsearch-certutil cert --ca elastic-stack-ca.p12
-green
-echo "Certificate successfully generated"
-normal
-# Enter
-# Enter
-# Enter
-
-blue
-echo
-echo "Downloading certificate..."
-normal
-kubectl cp monitoring/"$espod":/usr/share/elasticsearch/elastic-certificates.p12 elastic-certificates.p12
 
 blue
 echo
 echo "Creating kubernetes secret using certificate..."
 normal
-kubectl create secret generic elasticsearch-tls --from-file=elastic-certificates.p12 -n monitoring
+kubectl create secret generic elasticsearch-tls -n monitoring --from-file=elastic-certificate-bundle/ca/ca.crt --from-file=elastic-certificate-bundle/instance/tls.crt --from-file=elastic-certificate-bundle/instance/tls.key
 
 blue
 echo
@@ -85,12 +78,7 @@ echo
 echo "TLS successfully configured !"
 echo "Elasticsearch components are now restarting in order to take the new configuration into account"
 bold
-echo "Don't forget to save the generated certificate (monitoring/elastic-certificates.p12) in a safe place"
+echo "Don't forget to save the generated certificate (monitoring/elastic-certificate-bundle) in a safe place"
 normal
 
 read -p "Press enter to finish"
-
-
-openssl pkcs12 -in filename.pfx -nocerts -out filename.key
-
-openssl pkcs12 -in filename.pfx -clcerts -nokeys -out filename.crt

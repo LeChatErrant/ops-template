ec9567ac776074e76a34520f0332352627261902
LeChatErrant <guillaume.hector@epitech.eu>
2021-02-20 20:06:16 +0100

    feat(monitoring): heartbeat deployment, wip on config

---
+++ monitoring/heartbeat/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+bold() {
+  echo -en "\e[1m"
+}
+
+normal() {
+  echo -en "\e[0m"
+}
+
+green() {
+  echo -en "\e[32m"
+}
+
+yellow() {
+  echo -en "\e[33m"
+}
+
+blue() {
+  echo -en "\e[34m"
+}
+
+kubectl apply -f heartbeat.rbac.yaml
+kubectl apply -f heartbeat.configmap.yaml
+kubectl apply -f heartbeat.deployment.yaml

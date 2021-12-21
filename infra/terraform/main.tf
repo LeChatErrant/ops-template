fa852abcc92e45252560ba9aa3680819950c6698
LeChatErrant <guillaume.hector@epitech.eu>
2021-12-21 17:23:41 +0100

    feat(infra): using whode DO infra

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -18,3 +18,45 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
     max_nodes  = 5
   }
 }
+
+resource "digitalocean_loadbalancer" "load-balancer" {
+  name   = "load-balancer"
+  region = "fra1"
+
+  forwarding_rule {
+    entry_port      = 80
+    entry_protocol  = "tcp"
+    target_port     = 30000
+    target_protocol = "tcp"
+  }
+
+  forwarding_rule {
+    entry_port      = 443
+    entry_protocol  = "tcp"
+    target_port     = 30443
+    target_protocol = "tcp"
+  }
+
+  healthcheck {
+    port     = 30000
+    protocol = "tcp"
+  }
+}
+
+resource "digitalocean_domain" "domain" {
+  name = "lechaterrant.com"
+}
+
+resource "digitalocean_record" "traefik" {
+  domain = digitalocean_domain.domain.name
+  name   = "traefik"
+  type   = "A"
+  value  = digitalocean_loadbalancer.load-balancer.ip
+}
+
+resource "digitalocean_record" "argocd" {
+  domain = digitalocean_domain.domain.name
+  name   = "argocd"
+  type   = "A"
+  value  = "argocd.${digitalocean_domain.domain.name}"
+}

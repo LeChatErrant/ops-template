e185983975f7992affffb826fd0ed651e2c367ec
LeChatErrant <guillaume.hector@epitech.eu>
2021-12-21 01:34:32 +0100

    infra: kubernetes autoscaled cluster

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,3 +3,17 @@ resource "digitalocean_project" "ops-template" {
   description = "Infrastructure of the ops-template project"
   purpose     = "Web Application"
 }
+
+resource "digitalocean_kubernetes_cluster" "cluster" {
+  name    = "K8S cluster"
+  region  = "fra1"
+  version = "1.21.5-do.0"
+
+  node_pool {
+    name       = "autoscale-worker-pool"
+    size       = "s-2vcpu-2gb"
+    auto_scale = true
+    min_nodes  = 2
+    max_nodes  = 5
+  }
+}
